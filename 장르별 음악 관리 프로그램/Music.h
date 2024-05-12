615195f29ad03c84d5d02577c224df00ac328462
tumbaba <168721037+tumbaba@users.noreply.github.com>
2024-05-12 21:47:38 +0900

    음악 관리 프로그램

---
+++ 장르별 음악 관리 프로그램/Music.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "stdafx.h"
+#include "UI.h"
+
+class Music
+{
+private:
+	string genre;
+	string singer;
+	string song;
+	string lyrics;
+public:
+	int searchgenre;
+	int searchsinger;
+	int searchsong;
+	
+
+public:
+	Music() {}
+	Music(string g, string si, string so, string l, int a, int b, int c)
+		: genre(g), singer(si), song(so), lyrics(l), searchgenre(0), searchsinger(0), searchsong(0) {}
+	~Music() {}
+	void init(string g, string si, string so, string l);
+	void PrintGenre();
+	void Printsinger();
+	void Printsong();
+	void PrintLyrics();
+	void Printtotal();
+
+
+	string getgenre() { return genre; }
+	string getsinger() { return singer; }
+	string getsong() { return song; }
+	string getlyrics() { return lyrics; }
+
+	void setgenre(string sgenre) { genre = sgenre; }
+	void setsinger(string ssinger) { singer = ssinger; }
+	void setsong(string ssong) { song = ssong; }
+	void setlyrics(string slyrics) { lyrics = slyrics; }
+};
+
